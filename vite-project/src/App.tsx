699962d5251fb1b099ca1a45e2226c59bd670983
youssefmetwalli <ymetwalli22@gmail.com>
2023-12-26 13:58:14 +0900

    initial commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,52 @@
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { NewNote } from "./NewNote"
+import { useLocalStorage } from './UseLocalStorage'
+
+export type Note = {
+  id: string
+
+} & NoteData
+
+export type NoteData = {
+   title: string
+   markdown: string
+   tags: Tag[]
+}
+
+export type Tag = {
+  id: string
+  label: string
+}
+
+export type RawNote = {
+  id: string
+}
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[] //store the ids of our tags
+}
+
+function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  )
+}
+
+export default App
