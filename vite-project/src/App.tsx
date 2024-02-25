445533973be1dd1d8a241b803ba48d2809c19ea1
youssefmetwalli <ymetwalli22@gmail.com>
2024-02-25 18:29:35 +0900

    finalising project

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,28 +1,18 @@
-import './App.css'
-import "bootstrap/dist/css/bootstrap.min.css"
-import { Container } from "react-bootstrap"
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { NewNote } from "./NewNote"
-import { useLocalStorage } from './UseLocalStorage'
-import { useMemo } from 'react'
-import { v4 as uuidV4} from 'uuid'
-import { NoteList } from './NoteList'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useMemo } from "react";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { NewNote } from "./NewNote";
+import { useLocalStorage } from "./useLocalStorage";
+import { v4 as uuidV4 } from "uuid";
+import { NoteList } from "./NoteList";
+import { NoteLayout } from "./NoteLayout";
+import { Note } from "./Note";
+import { EditNote } from "./EditNote";
 
 export type Note = {
   id: string
-
 } & NoteData
-
-export type NoteData = {
-   title: string
-   markdown: string
-   tags: Tag[]
-}
-
-export type Tag = {
-  id: string
-  label: string
-}
 
 export type RawNote = {
   id: string
@@ -31,34 +21,115 @@ export type RawNote = {
 export type RawNoteData = {
   title: string
   markdown: string
-  tagIds: string[] //store the ids of our tags
+  tagIds: string[]
+}
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Tag = {
+  id: string
+  label: string
 }
 
 function App() {
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
   const notesWithTags = useMemo(() => {
     return notes.map(note => {
-      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
-    }) 
-  }, [notes, tags]
-  )
-  function onCreateNote({tags, ...data}: NoteData) {
-    setNotes( prevNotes => {
-      return [...prevNotes, { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) }] //uuid creates a string based id that will always be unique
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
+    })
+  }, [notes, tags])
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes(prevNotes => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) },
+      ]
     })
   }
+
+  function onUpdateNote(id: string, { tags, ...data }: NoteData) {
+    setNotes(prevNotes => {
+      return prevNotes.map(note => {
+        if (note.id === id) {
+          return { ...note, ...data, tagIds: tags.map(tag => tag.id) }
+        } else {
+          return note
+        }
+      })
+    })
+  }
+
+  function onDeleteNote(id: string) {
+    setNotes(prevNotes => {
+      return prevNotes.filter(note => note.id !== id)
+    })
+  }
+
   function addTag(tag: Tag) {
     setTags(prev => [...prev, tag])
   }
+
+  function updateTag(id: string, label: string) {
+    setTags(prevTags => {
+      return prevTags.map(tag => {
+        if (tag.id === id) {
+          return { ...tag, label }
+        } else {
+          return tag
+        }
+      })
+    })
+  }
+
+  function deleteTag(id: string) {
+    setTags(prevTags => {
+      return prevTags.filter(tag => tag.id !== id)
+    })
+  }
+
   return (
     <Container className="my-4">
       <Routes>
-        <Route path="/" element={<NoteList />} />
-        <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag= {addTag} availableTags={tags}/>} />
-        <Route path="/:id">
-          <Route index element={<h1>Show</h1>} />
-          <Route path="edit" element={<h1>Edit</h1>} />
+        <Route
+          path="/"
+          element={
+            <NoteList
+              notes={notesWithTags}
+              availableTags={tags}
+              onUpdateTag={updateTag}
+              onDeleteTag={deleteTag}
+            />
+          }
+        />
+        <Route
+          path="/new"
+          element={
+            <NewNote
+              onSubmit={onCreateNote}
+              onAddTag={addTag}
+              availableTags={tags}
+            />
+          }
+        />
+        <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
+          <Route index element={<Note onDelete={onDeleteNote} />} />
+          <Route
+            path="edit"
+            element={
+              <EditNote
+                onSubmit={onUpdateNote}
+                onAddTag={addTag}
+                availableTags={tags}
+              />
+            }
+          />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
--- vite-project/src/NewNote.tsx
+++ vite-project/src/NewNote.tsx
@@ -1,18 +1,20 @@
-import { NoteData } from "./App"
-import { NoteForm } from "./NoteForm"
-
+import { NoteData, Tag } from "./App";
+import { NoteForm } from "./NoteForm";
 type NewNoteProps = {
-    onSubmit: (data: NoteData) => void
-    onAddTag: (tag: Tag) => void
+  onSubmit: (data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
 }
 
-
-export function NewNote({ onSubmit}: NewNoteProps) {
-    return (
-
-        <>
-        <h1 className="mb-4"> New Note</h1>
-        <NoteForm onSubmit={onSubmit} />
-        </>
-    )
+export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
+  return (
+    <>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </>
+  )
 }
